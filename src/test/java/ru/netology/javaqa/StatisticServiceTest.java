118b4b7e5a0c9aab6979c38cb9efd57662db201e
Eugen Sabanovskiy <eugensabanovskiy123@gmail.com>
2024-12-06 21:38:16 +0300

    Another commit

--- src/test/java/ru/netology/javaqa/StatisticServiceTest.java
+++ src/test/java/ru/netology/javaqa/StatisticServiceTest.java
@@ -10,7 +10,7 @@ public class StatisticServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 120;
 
         long actual = service.findMax(incomesInBillions);
 
